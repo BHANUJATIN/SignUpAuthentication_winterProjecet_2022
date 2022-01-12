d4a554e09ace03c3d7b10ea5bf33f228ee32f87a
BHANUJATIN <bjsbainsla@gmail.com>
2022-01-12 16:42:07 +0530

    updated

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
-// const targetDiv = document.getElementById("third");
-// const btn = document.getElementById("toggle");
-// btn.onclick = function () {
-//   if (targetDiv.style.display !== "none") {
-//     targetDiv.style.display = "none";
-//   } else {
-//     targetDiv.style.display = "block";
-//   }
-// };
 
 const loginPage = document.querySelector('.login-section');
 const signUpPage = document.querySelector('.signUp-section');
+const loginBanner = document.getElementById("login-Banner");
+const signUpBanner = document.getElementById("signUp-Banner");
 const btns = document.querySelectorAll('.toggle');
-console.log(btns);
+
+
 btns.forEach(btn => btn.addEventListener('click', () => {
-    if(btn.dataset.name == 'signUp'  ) 
-        {loginPage.style.display = 'none';
-        signUpPage.style.display = 'block';}
-    if(btn.dataset.name == 'login')
-        {loginPage.style.display = 'block';
-        signUpPage.style.display = 'none';}
+    if(btn.dataset.name == 'signUp'  ) {
+        loginPage.style.display = 'none';
+        signUpPage.style.display = 'flex';
+        loginBanner.style.display = 'none';
+        signUpBanner.style.display = 'flex'; }
+
+    if(btn.dataset.name == 'login') {
+        loginPage.style.display = 'flex';
+        signUpPage.style.display = 'none';
+        loginBanner.style.display = 'flex';
+        signUpBanner.style.display = 'none'; }
 
 } ))
